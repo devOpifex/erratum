4c2f02a23ce901379d218db5371c285e762b6b04
JohnCoene <jcoenep@gmail.com>
2020-11-29 16:07:51 +0100

    init

---
+++ R/err.R
@@ -0,0 +1,52 @@
+#' Error & Warnings
+#' 
+#' Create new errors and warnings.
+#' 
+#' @param error Message string, object of class `error`,
+#' or `warning`.
+#' @param ... Ignored.
+#' @param type Type of the message `error` or `warning`. 
+#'
+#' @export
+new <- function(error, ..., type = c("error", "warning")) UseMethod("new")
+
+#' @export 
+new.character <- function(error, ..., type = c("error", "warning")){
+  stopifnot(!missing(error))
+  construct_error(error, type)
+}
+
+#' @method new error
+#' @export 
+new.error <- function(error, ...){
+  stopifnot(!missing(error))
+  construct_error(error$message, "error")
+}
+
+#' @method new warning
+#' @export 
+new.warning <- function(error, ...){
+  stopifnot(!missing(error))
+  construct_error(error$message, "warning")
+}
+
+construct_error <- function(error, type = c("error", "warning")){
+  type <- match.arg(type)
+
+  obj <- list(message = error, type = type)
+  structure(obj, class = c("err", "simpleError", class(obj)))
+}
+
+#' @export 
+print.err <- function(x, ...){
+  fn <- type2func(x$type)
+  fn(x$message)
+}
+
+type2func <- function(type = c("error", warning)){
+  switch(
+    type,
+    error = cli::cli_alert_danger,
+    warning = cli::cli_alert_warning
+  )
+}
