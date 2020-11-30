075e29c58752012ee8d8b065a1cb1cb5f2f163da
JohnCoene <jcoenep@gmail.com>
2020-11-30 22:13:05 +0100

    add tests on latch

--- tests/testthat/test_latch.R
+++ tests/testthat/test_latch.R
@@ -1,9 +1,20 @@
 test_that("latch", {
+
+  testthat::expect_error(latche())
+  testthat::expect_error(latchw())
+
   x <- 1
+
+  testthat::expect_error(latche(x))
+  testthat::expect_error(latchw(x))
+  testthat::expect_error(latche(x, "error"))
+  testthat::expect_error(latchw(x, "warning"))
 
   le <- latche(x, e("Probematic"))
   lw <- latchw(x, w("Suspicious"))
 
+  testthat::expect_snapshot_output(le)
+  testthat::expect_snapshot_output(lw)
   testthat::expect_true(is.e(le))
   testthat::expect_true(is.w(lw))
   testthat::expect_error(enforce(le))
